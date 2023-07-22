14316aa93385bf4a1718778900216f7a53046be0
Rafael450 <rafaelcamargo633@gmail.com>
2023-07-22 22:13:15 +0200

    feat: finished deployment script

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,9 +1,55 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
+dotenv.config({ path: __dirname+'/.env' });
+const myPrivateKey: string = process.env.PRIVATE_KEY as string;
+const myInfuraId: string = process.env.DEPLOY_INFURA_KEY as string;
+
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity:  {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    moonbase: {
+      url: 'https://rpc.api.moonbase.moonbeam.network',
+    },
+    aurora: {
+      url: `https://aurora-testnet.infura.io/v3/${myInfuraId}`,
+      accounts: [myPrivateKey],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${myInfuraId}`,
+      accounts: [myPrivateKey],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${myInfuraId}`,
+      accounts: [myPrivateKey],
+    },
+    moonbeam: {
+      url: 'https://rpc.api.moonbeam.network',
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.DEPLOY_INFURA_KEY}`
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.DEPLOY_INFURA_KEY}`
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.DEPLOY_INFURA_KEY}`
+    }
+  },
+  paths: {
+    artifacts: "./build"
+  }
 };
 
 export default config;
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,22 +1,29 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  
+    const lockedAmount = ethers.parseEther("1");
+  
+    const ChainFund6551 = await ethers.getContractFactory("ChainFund6551");
+    const chainFund6551 = await ChainFund6551.deploy();
+    
+    await chainFund6551.waitForDeployment();
+    console.log("ERC6551 address:", chainFund6551.target);
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    const ERC6551Registry = await ethers.getContractFactory("ERC6551Registry");
+    const eRC6551Registry = await ERC6551Registry.deploy();
+    
+    await eRC6551Registry.waitForDeployment();
+    console.log("Regestry address:", eRC6551Registry.target);
+  
+    const ChainFund721 = await ethers.getContractFactory("ChainFund");
+    const chainFund721 = await ChainFund721.deploy(eRC6551Registry.target, chainFund6551.target, "0xE592427A0AEce92De3Edee1F18E0157C05861564");
+  
+    await chainFund721.waitForDeployment();
+    console.log("ERC721 address:", chainFund721.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
